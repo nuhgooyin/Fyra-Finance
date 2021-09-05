4ca895c373d2f09ec05fc37b4032a59cce4edc06
--replace-all <dannguyenbox@gmail.com>
2021-09-04 23:34:06 -0400

    try

--- calculators/compoundInterestCalc.py
+++
@@ -1,8 +0,0 @@
-# Compounding Interest Calculator MD
-# Formula: final amount = (initial balance) * (1 + interest rate / # compounding periods) ^ (# compounding periods * time period elapsed)
-def compoundInterest(principle, interestPercent, period, time):
-
-    interestDecimal = interestPercent / 100
-    final = principle * (1 + interestDecimal/period) ** (period * time)
-
-    return final
--- investmentOptions/stocks.py
+++
@@ -1,3 +0,0 @@
-# Stocks investment options template
-# Includes various stock investments (ETFs, index funds, mutual funds, etc.)
-# Characteristics of each investment options (expense ratio, fees, returns, etc.)
